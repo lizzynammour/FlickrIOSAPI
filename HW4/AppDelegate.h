e095141fa6524d77e32d5156bd0f0593737a035f
elizabethnammour <lizzynammour@gmail.com>
2015-10-17 17:36:46 -0400

    Initial Commit

---
+++ HW4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW4
+//
+//  Created by Lizzy Nammour on 10/17/15.
+//  Copyright (c) 2015 enammour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW4
+//
+//  Created by Lizzy Nammour on 10/17/15.
+//  Copyright (c) 2015 enammour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
